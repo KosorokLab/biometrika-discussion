b1ab8f829f9c22ae8e5b5b8ed340cbd511d96427
Kushal Shah <31373302+kshah1996@users.noreply.github.com>
2020-10-05 17:42:17 -0400

    beta_hat_trajectory outputs trajectory now

---
+++ beta_hat_trajectory.R
@@ -0,0 +1,35 @@
+source("dgm_binary_categorical_covariate.R")
+source("estimators.R")
+
+set.seed(2020)
+
+control_vars <- "S"
+moderator_vars <- "S"
+delta <- 6
+
+dta_orig <- data_generating_process(500, 10)
+dta_updated <- dgm_update(dta_orig)
+
+beta_hats <- matrix(nrow = delta, ncol = 2, data = NA)
+colnames(beta_hats) <- c("intercept", "slope")
+
+for (i in 1:delta) {
+  dta_delta <- as.data.frame(dgm_delta(dta_updated, del = i))
+  fit <- weighted_centered_least_square(
+    dta = dta_delta,
+    id_varname = "userid",
+    decision_time_varname = "day",
+    treatment_varname = "A",
+    outcome_varname = "Y_trajectory",
+    control_varname = control_vars,
+    importance_varname = "importance",
+    moderator_varname = moderator_vars,
+    rand_prob_varname = "prob_A",
+    rand_prob_tilde_varname = NULL,
+    rand_prob_tilde = 0.2,
+    estimator_initial_value = NULL
+  )
+  beta_hats[i, ] <- fit$beta_hat
+}
+
+print(beta_hats)
